9f08b09551b8e93c3a207d1feadf4fa17646f6fb
Jacob Salzberg <jsalzbergedu@yahoo.com>
2017-04-01 18:30:04 -0400

    Cleaned up the code, added some functionality

--- main.rs
+++ main.rs
@@ -1,201 +1,66 @@
-// Stolen with love from orbutils' editor
+// Stolen with love from orbutils' editor 
 //#![deny(warnings)]
 
 extern crate orbclient;
 extern crate orbtk;
 extern crate extra;
+extern crate coreutils;
 
-use orbtk::{Action, Button, Menu, Point, Rect, Separator, TextBox, Window};
-use orbtk::traits::{Click, Place, Text};
-
-use std::{cmp, env};
-
-
-
+use orbtk::{Rect, Window};
+use std::cmp;
 use::std::error::Error;
-use::std::io::prelude::*;
 use std::path::Path;
-
 use std::fs;
 use std::fs::File;
-use std::io::{stdout, stderr, Write, Read};
-use std::process::exit;
-
+use std::io::{stderr, Write};
 use extra::io::fail;
 use extra::option::OptionalExt;
+use std::env;
+use coreutils::ArgParser;
+
+fn setup_mode() {
+    // Must be run w/ superuser. Replaces orbital.conf with one that points to /home/user/.themes. (In theory)
+    let mut stderr = stderr();
+    let default_conf_string = "background=/ui/background.png
+background_mode=zoom
+cursor=/home/user/.themes/left_ptr.png
+bottom_right_corner=/ui/bottom_right_corner.png
+bottom_side=/ui/bottom_side.png
+right_side=/ui/right_side.png
+window_max=/ui/window_max.png
+window_max_unfocused=/ui/window_max_unfocused.png
+window_close=/home/user/.themes/window_close.png
+window_close_unfocused=/ui/window_close_unfocused.png";
+    let mut orbital_conf_source_path = Path::new("/ui/orbital.conf");
+    let mut default_conf = File::create("/ui/orbital.conf").try(&mut stderr);
+    default_conf.write(default_conf_string.as_bytes()).try(&mut stderr);
+    fs::create_dir(Path::new("/home/user/.themes/")).try(&mut stderr);
+    File::create(Path::new("/home/user/.themes/left_ptr.png")).try(&mut stderr);
+    File::create(Path::new("home/user/.themes/window_close.png")).try(&mut stderr);
+    fs::copy(Path::new("/home/user/left_ptr.png"), Path::new("/home/user/.themes/left_ptr.png")).try(&mut stderr);
+    fs::copy(Path::new("/home/user/window_close.png"), Path::new("/home/user/.themes/window_close.png")).try(&mut stderr);
+}
 
 
-/*
-fn main(){
-    let path_option = env::args().nth(1);
-
-    let title = if let Some(ref path) = path_option {
-        format!("{} - Editor", path)
-    } else {
-        format!("Editor")
-    };
-
+fn normal_mode() {
+    let title = { format!("Themer") };
     let (display_width, display_height) = orbclient::get_display_size().expect("viewer: failed to get display size");
     let (width, height) = (cmp::min(800, display_width * 4/5), cmp::min(576, display_height * 4/5));
-
     let mut window = Window::new(Rect::new(-1, -1, width, height), &title);
-
-    let text_box = TextBox::new();
-    text_box.position(0, 16)
-        .size(width, height - 16);
-    window.add(&text_box);
-
-    if let Some(ref path) = path_option {
-        match File::open(path) {
-            Ok(mut file) => {
-                let mut text = String::new();
-                match file.read_to_string(&mut text) {
-                    Ok(_) => text_box.text.set(text),
-                    Err(err) => println!("Failed to read {}: {}", path, err)
-                }
-            },
-            Err(err) => println!("Failed to open {}: {}", path, err)
-        }
-    }
-
-    let menu = Menu::new("File");
-    menu.position(0, 0).size(32, 16);
-
-    let open_action = Action::new("Open");
-    open_action.on_click(|_action: &Action, _point: Point| {
-        println!("Open");
-    });
-    menu.add(&open_action);
-
-    menu.add(&Separator::new());
-
-    let save_action = Action::new("Save");
-    let save_path_option = path_option.clone();
-    let save_text_box = text_box.clone();
-    save_action.on_click(move |_action: &Action, _point: Point| {
-        println!("Save");
-        if let Some(ref path) = save_path_option {
-            println!("Create {}", path);
-            match File::create(path) {
-                Ok(mut file) => {
-                    let text = save_text_box.text.borrow();
-                    match file.write(&text.as_bytes()) {
-                        Ok(_) => match file.set_len(text.len() as u64) {
-                            Ok(_) => println!("Successfully saved {}", path),
-                            Err(err) => println!("Failed to truncate {}: {}", path, err)
-                        },
-                        Err(err) => println!("Failed to write {}: {}", path, err)
-                    }
-                },
-                Err(err) => println!("Failed to open {}: {}", path, err)
-            }
-        } else {
-            println!("Need to create file!");
-        }
-    });
-    menu.add(&save_action);
-
-    let save_as_action = Action::new("Save As");
-    let save_as_path_option = path_option.clone();
-    save_as_action.on_click(move |_action: &Action, _point: Point| {
-        println!("Save As");
-        let mut window = Window::new(Rect::new(-1, -1, 320, 32), "Save As");
-
-        let text_box = TextBox::new();
-        text_box.position(0, 0)
-            .size(320, 16);
-        window.add(&text_box);
-
-        if let Some(ref path) = save_as_path_option {
-            text_box.text.set(path.clone());
-        }
-
-        {
-            let window_cancel = &mut window as *mut Window;
-            let button = Button::new();
-            button.position(0, 16)
-                .size(320/2, 16)
-                .text("Cancel")
-                .on_click(move |_button: &Button, _point: Point| {
-                    unsafe { (&mut *window_cancel).close(); }
-                });
-            window.add(&button);
-        }
-
-        {
-            let window_save_as = &mut window as *mut Window;
-            let button = Button::new();
-            button.position(320/2, 16)
-                .size(320/2, 16)
-                .text("Save As")
-                .on_click(move |_button: &Button, _point: Point| {
-                    println!("Save {}", text_box.text.get());
-                    unsafe { (&mut *window_save_as).close(); }
-                });
-            window.add(&button);
-        }
-
-        window.exec();
-    });
-    menu.add(&save_as_action);
-
-    menu.add(&Separator::new());
-
-    let close_action = Action::new("Close");
-    let window_close = &mut window as *mut Window;
-    close_action.on_click(move |_action: &Action, _point: Point| {
-        println!("Close");
-        unsafe { (&mut *window_close).close(); }
-    });
-    menu.add(&close_action);
-
-    window.add(&menu);
-
     window.exec();
-}
-*/
-
-struct OrbitalConfFile {
-    // TODO: Figure out how to use less Strings
-    
-    background: String,
-    background_mode: String,
-    cursor: String,
-    bottom_right_corner: String,
-    bottom_side: String,
-    right_side: String,
-    window_max: String,
-    window_max_unfocused: String,
-    window_close: String,
-    window_close_unfocused: String,
-    // orbclient_theme: String, // One can hope
 }
 
 fn main() {
     // Why doesn't this work????
     // Is File::create even implemented? I'm not sure that it is working on the "editor"
-    let default_conf_string = "background=/ui/background.png\nbackground_mode=zoom\ncursor=/ui/left_ptr.png\nbottom_right_corner=/ui/bottom_right_corner.png\nbottom_side=/ui/bottom_side.png\nright_side=/ui/right_side.png\nwindow_max=/ui/window_max.png\nwindow_max_unfocused=/ui/window_max_unfocused.png\nwindow_close=/ui/window_close.png\nwindow_close_unfocused=/ui/window_close_unfocused.png";
-    let title = { format!("Themer") };
-    let (display_width, display_height) = orbclient::get_display_size().expect("viewer: failed to get display size");
-    let (width, height) = (cmp::min(800, display_width * 4/5), cmp::min(576, display_height * 4/5));
-    let mut window = Window::new(Rect::new(-1, -1, width, height), &title);
-    let mut orbital_conf_source_path = Path::new("/ui/orbital.conf");
     let mut stderr = stderr();
-/*    if !orbital_conf_source_path.exists() {
-        let mut default_conf = match File::create(orbital_conf_source_path) {
-            Err(why) => panic!("Error in creating default orbital.conf, why: {}", why.description()),
-            Ok(default_conf) => default_conf,
-        };
-        match default_conf.write_all(default_conf_string.as_bytes()) {
-            Err(why) => {panic!("Error in writing to default orbital.conf, why: {}", why.description())},
-            Ok(_) => println!("Wrote default options to /ui/orbital.conf"),
-        }
+    let mut parser = ArgParser::new(1).add_flag(&["setup","setup"]);
+    parser.parse(env::args());
+    if parser.found("setup") {
+        setup_mode();
+    } else {
+        normal_mode();
     }
-     */
-    if !orbital_conf_source_path.exists() {
-        let mut default_conf = File::create("/ui/orbital.conf").try(&mut stderr);
-        default_conf.write(default_conf_string.as_bytes()).try(&mut stderr);
-    }
-    window.exec();
+
 }
 
--- readme.md
+++ readme.md
@@ -2,13 +2,14 @@
 Themer is intended to change the look of redox' graphical user interface, namely, orbital, orbital's wiget toolkit, and orbclient.
 
 ## Installation
-Install redox using the instructions for manual setup, then clone this repository. Replace `redox/programs/orbutils/Cargo.toml`,
-as well as `redox/mk/userspace/orbutils`, with the files in this repo. Make a directory `redox/orbutils/themer`, and put the `main.rs`
-of this repo in that directory.
+Under redox/programs, make a directory themer. Copy the Cargo.toml to the directory, and make a directory /src. Copy the main.rs
+to the directory. Go up to redox/Cargo.toml, and, in a line under `"programs/userutils",`, a line `"programs/themer",`.
+Then, in the file redox/mk/userspace/mod.mk, add, in a line under `filesystem/bin/tar \` (making sure to add the backslash),
+`filesystem/bin/themer \`. Copy the window\_close and left\_ptr files from redox/filesystem/ui to redox/filesystem/home/user/. Run `make` and `make qemu`.
 
 ## Running
 Run `make qemu` from your redox directory. Open the terminal in redox, and run `cd /ui/bin`, `sudo ./themer`.
 
 ## Roadmap
-This program currently does nothing, but I hope to have it change the default /ui/orbital.conf as well as the default icons,
+This program currently does not do much, but I hope to have it change the default /ui/orbital.conf as well as the default icons,
 and, if themes are ever implemented as some part of redox' orbital, configure the themes.
